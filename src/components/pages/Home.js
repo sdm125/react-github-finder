2b26d824ec8666f078495c7959d30dbd8e2deda7
Stacy Mackin <stacy.mackin@gmail.com>
2020-01-12 17:26:17 -0500

    Refactor using context API

---
+++ src/components/pages/Home.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+import Search from '../users/Search';
+import Users from '../users/Users';
+
+export default () => (
+  <Fragment>
+    <Search />
+    <Users />
+  </Fragment>
+);
